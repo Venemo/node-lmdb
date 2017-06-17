dd60e8ab0d78effafa185a47bdb20464e39640a2
Dmitriy Gorokh <degifted@gmail.com>
2017-06-17 22:58:24 +0300

    Update dbi.cpp

--- src/dbi.cpp
+++ src/dbi.cpp
@@ -138,6 +138,9 @@ NAN_METHOD(DbiWrap::ctor) {
     }
     else {
         isOpen = true;
+        if (keyType == NodeLmdbKeyType::StringKey) {
+            rc = mdb_set_compare(txn, dbi, utf16_cmp_func);
+        }
     }
 
     if (needsTransaction) {
