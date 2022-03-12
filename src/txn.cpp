97760104c0fd311206b88aecd91fa1f59fe2b85a
Kris Zyp <kriszyp@gmail.com>
2022-03-12 07:19:20 -0700

    Perform argument check for putString

--- src/txn.cpp
+++ src/txn.cpp
@@ -295,6 +295,8 @@ Nan::NAN_METHOD_RETURN_TYPE TxnWrap::putCommon(Nan::NAN_METHOD_ARGS_TYPE info, v
 }
 
 NAN_METHOD(TxnWrap::putString) {
+    if (!info[2]->IsString())
+        return Nan::ThrowError("Value must be a string.");
     return putCommon(info, [](Nan::NAN_METHOD_ARGS_TYPE info, MDB_val &data) -> void {
         CustomExternalStringResource::writeTo(Local<String>::Cast(info[2]), &data);
     }, [](MDB_val &data) -> void {
